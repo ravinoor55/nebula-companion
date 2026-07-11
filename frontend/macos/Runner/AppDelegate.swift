35fb9405b2d7da892039fcf60d1668471f4b91da
ravinoor55 <ravinoorsingh27441@gmail.com>
2026-07-11 19:03:38 +0530

    Refactor: Swap NSPopover for custom NSPanel to fix text input focus

--- frontend/macos/Runner/AppDelegate.swift
+++ frontend/macos/Runner/AppDelegate.swift
@@ -1,9 +1,14 @@
 import Cocoa
 import FlutterMacOS
 
+class FloatingPanel: NSPanel {
+    override var canBecomeKey: Bool { return true }
+    override var canBecomeMain: Bool { return true }
+}
+
 @main
 class AppDelegate: FlutterAppDelegate {
-    var popover: NSPopover!
+    var panel: FloatingPanel!
     var statusBarItem: NSStatusItem!
 
     override func applicationDidFinishLaunching(_ aNotification: Notification) {
@@ -12,29 +17,42 @@ class AppDelegate: FlutterAppDelegate {
             return
         }
         
-        popover = NSPopover()
-        popover.contentSize = NSSize(width: 400, height: 600)
-        popover.behavior = .transient
-        popover.contentViewController = flutterViewController
+        panel = FloatingPanel(
+            contentRect: NSRect(x: 0, y: 0, width: 400, height: 600),
+            styleMask: [.nonactivatingPanel, .titled, .fullSizeContentView],
+            backing: .buffered,
+            defer: false
+        )
+        panel.titlebarAppearsTransparent = true
+        panel.titleVisibility = .hidden
+        panel.isOpaque = false
+        panel.backgroundColor = .clear
+        panel.contentViewController = flutterViewController
 
         statusBarItem = NSStatusBar.system.statusItem(withLength: NSStatusItem.variableLength)
         if let button = statusBarItem.button {
             button.image = NSImage(systemSymbolName: "sparkles", accessibilityDescription: "Nebula")
-            button.action = #selector(togglePopover(_:))
+            button.action = #selector(togglePanel(_:))
         }
 
         // Hide default window instead of closing it
         window.orderOut(nil)
     }
 
-    @objc func togglePopover(_ sender: AnyObject?) {
+    @objc func togglePanel(_ sender: AnyObject?) {
         if let button = statusBarItem.button {
-            if popover.isShown {
-                popover.performClose(sender)
+            if panel.isVisible {
+                panel.orderOut(nil)
             } else {
+                if let window = button.window {
+                    let buttonRect = window.convertToScreen(button.convert(button.bounds, to: nil))
+                    let x = buttonRect.midX - panel.frame.width / 2
+                    let y = buttonRect.minY - panel.frame.height
+                    panel.setFrameOrigin(NSPoint(x: x, y: y))
+                }
+                
                 NSApp.activate(ignoringOtherApps: true)
-                popover.show(relativeTo: button.bounds, of: button, preferredEdge: NSRectEdge.minY)
-                self.popover.contentViewController?.view.window?.makeKey()
+                panel.makeKeyAndOrderFront(nil)
             }
         }
     }
